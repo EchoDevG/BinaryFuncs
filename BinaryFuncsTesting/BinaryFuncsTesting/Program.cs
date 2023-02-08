e3ff70060d132cfd78d4c35cb47f5f98af1a4185
EchoDevG <75668023+EchoDevG@users.noreply.github.com>
2023-02-08 13:51:19 +0000

    Add files via upload

---
+++ BinaryFuncsTesting/BinaryFuncsTesting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using BinaryFuncs;
+
+namespace BinaryFuncsTesting
+{
+    public class Program
+    {
+        static void Main()
+        {
+            TwosComplimentBinary bin = new TwosComplimentBinary();
+            bin.setBinaryWithDenary(-47);
+
+            Console.WriteLine(bin.getBinary());
+
+            bin.changeSign();
+
+            Console.WriteLine(bin.getBinary());
+
+            Console.ReadKey();
+        }
+    }
+}
